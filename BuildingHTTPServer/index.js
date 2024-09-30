ebdb91b5a0e4d42f7c7636acbad79c61db5872a8
Hammad <hammadaslam382@gmail.com>
2024-09-30 22:09:11 +0500

    Handling URLs

--- BuildingHTTPServer/index.js
+++ BuildingHTTPServer/index.js
@@ -1,28 +1,34 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
-const myServer = http.createServer((req, res) =>{
-const log = `${Date.now()}, ${req.url}\n New Request Recieved! \n`;
+const myServer = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') return res.end();
+  const log = `${Date.now()}, ${req.url}\n New Request Recieved! \n`;
+  const myUrl = url.parse(req.url, true)
+  console.log(myUrl);
 
 
-fs.appendFile('log.txt', log, (err, data) =>{
-switch(req.url){
-case '/':
-res.end('Home Page');
-break
-case '/about':
-res.end('I amd Hammad Aslam!!')
-break
-default:
-res.end('404 Not found')
-}
-console.log('New Request Recieved');
-})
+  fs.appendFile('log.txt', log, (err, data) => {
+    switch (myUrl.pathname)
+    {
+      case '/':
+        res.end('Home Page');
+        break
+      case '/about':
+        const username = myUrl.query.myname;
+        res.end(`hi, ${username}`)
+        break
+      default:
+        res.end('404 Not found')
+    }
+    console.log('New Request Recieved');
+  })
 
 })
 
 myServer.listen(8000, () =>
-console.log('Server Started!')
+  console.log('Server Started!')
 
 )
 
